ac8e43b86179d1850125da338a217fd17830eb6e
maxime <maxime.theveneau@gmail.com>
2023-03-21 16:30:40 +0100

    add webView

--- app/src/main/java/com/example/belfunfair/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/belfunfair/ui/map/MapFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.belfunfair.R
 import com.example.belfunfair.databinding.FragmentMapBinding
 
 
@@ -34,7 +36,26 @@ class MapFragment : Fragment() {
         val title: TextView = binding.mapTitle
         mapViewModel.title.observe(viewLifecycleOwner) { title.text = it }
 
-        val map: ImageView = binding.map
+        //val map: ImageView = binding.map
+
+        val webView: WebView = binding.webview
+        val svg = resources.openRawResource(R.raw.map).bufferedReader().use { it.readText() }
+        val html = """
+            <html>
+                <head>
+                    <style>
+                        svg{height:100%; width:100%;}
+                    </style>
+                </head>
+                <body>
+                    <h1>Map</h1>
+                    <div style="position:absolute; top:0; left:0;">
+                        $svg
+                    </div>
+                </body>
+            </html>
+        """.trimIndent()
+        webView.loadDataWithBaseURL("file:///android_asset/", html, "text/html", "UTF-8", null)
 
     }
 
