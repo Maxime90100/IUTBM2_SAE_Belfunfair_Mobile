063261ce9d0d60c95088919062af5f845cc3f4f0
Bilel <bilel91.bouhedda@gmail.com>
2023-03-30 09:47:15 +0200

    Connexion button redirection to google

--- app/src/main/java/com/example/belfunfair/MainActivity.kt
+++ app/src/main/java/com/example/belfunfair/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.belfunfair
 
 import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
 import android.view.ContextMenu
 import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.Button
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SwitchCompat
+import androidx.core.net.toUri
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.belfunfair.databinding.ActivityMainBinding
@@ -35,14 +40,30 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.options_menu, menu);
         return super.onCreateOptionsMenu(menu)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.connexionFragment -> {
+                val url = "https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fgoogle%2Fcallback&client_id=606419346830-prkq4eqoskvqlb86tomev0q8igi879oi.apps.googleusercontent.com&access_type=offline&response_type=code&prompt=consent&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&state=http%3A%2F%2Flocalhost%3A8080%2Flogin&service=lso&o2v=2&flowName=GeneralOAuthFlow"
+                val i = Intent(Intent.ACTION_VIEW).apply {
+                    data = url.toUri()
+                }
+                startActivity(i)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
     fun isDarkMode(context: Context): Boolean {
         val darkModeFlag = context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
         return darkModeFlag == Configuration.UI_MODE_NIGHT_YES
     }
+
+
 }
